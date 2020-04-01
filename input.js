e841c370e14e51033371924108dc58fe4a3b8867
IlyaIvanchikov <phantomonster1@mail.ru>
2020-04-01 17:46:13 +0300

    feat: add folder data

--- input.js
+++
@@ -1,8 +0,0 @@
-const colors = require('colors');
-const text = 'This is secret. Message about symbol!';
-
-module.exports = function () {
-  console.log('Caesar cipher CLI tool');
-  console.log('------------------');
-  console.log('%s', colors.red(text));
-};
