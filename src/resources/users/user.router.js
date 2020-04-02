2d99f4d47e3c4c7319f67779915f137dbbbb8c08
IlyaIvanchikov <phantomonster1@mail.ru>
2020-04-02 20:32:54 +0300

    feat: add put and getId

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,22 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
   res.json(users.map(User.toResponse));
 });
 
+router.route('/:id').get(async (req, res) => {
+  const users = await usersService.getUser(req.params.id);
+  res.json(User.toResponse(users));
+});
+
+router.route('/').post(async (req, res) => {
+  const user = new User(req.body);
+  await usersService.saveUser(user);
+});
+
+router.route('/:id').put(async (req, res) => {
+  const user = new User(req.body);
+  const id = req.params.id;
+  await usersService.updateUser(id, user);
+});
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,7 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
-
-module.exports = { getAll };
+const saveUser = user => usersRepo.saveUser(user);
+const getUser = id => usersRepo.getUser(id);
+const updateUser = (id, user) => usersRepo.updateUser(id, user);
+module.exports = { getAll, saveUser, getUser, updateUser };
