36ed8f8de0d30cbcf161f3bbd5864627376c26dd
IlyaIvanchikov <phantomonster1@mail.ru>
2020-04-16 17:44:26 +0300

    feat: add connect mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const { PORT } = require('./common/config');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const app = require('./app');
 const logger = require('./common/logger');
-
+const mongoose = require('mongoose');
 process
   .on('uncaughtException', error => {
     logger.error(`captured error: ${error.message}`);
@@ -10,6 +10,20 @@ process
     logger.error(`Unhandled rejection detected: ${reason.message}`);
   });
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const start = async () => {
+  await mongoose.connect(MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log("We're connected!");
+  });
+  app.listen(PORT, () =>
+    console.log(`App is running on http://localhost:${PORT}`)
+  );
+};
+
+start();
